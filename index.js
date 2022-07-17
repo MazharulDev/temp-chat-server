91dc1ac34773c0fade6dd8259425fa263b94f620
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-07-17 08:56:40 +0600

    send message with console log

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ io.on("connection", (socket) => {
     socket.on("join_room", (data) => {
         socket.join(data);
         console.log(`User with id: ${socket.id} joined room: ${data}`);
+    });
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
     })
     socket.on("disconnect", () => {
         console.log("User disconnected", socket.id);
